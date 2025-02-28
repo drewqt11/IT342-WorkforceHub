7db8279152f388de5824b9d125e0696274fb5165
drewqt11 <andriapas4@gmail.com>
2025-03-01 04:02:14 +0800

    Tried layouting and button animations

--- frontend-mobile/Mobile/app/src/main/java/cit/edu/workforce/MainActivity.kt
+++ frontend-mobile/Mobile/app/src/main/java/cit/edu/workforce/MainActivity.kt
@@ -1,7 +1,7 @@
 package cit.edu.workforce
 
-import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
