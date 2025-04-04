527b8f9c854a0f436c6aba4d4e9efbe2768a9510
katkatty <katamores2104@gmail.com>
2025-04-04 14:10:58 +0800

    trial

---
+++ backend/workforce/src/main/java/cit/edu/workforce/config/DatabaseConfig.java
@@ -0,0 +1,21 @@
+package cit.edu.workforce.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+
+import javax.sql.DataSource;
+
+@Configuration
+public class DatabaseConfig {
+
+    @Bean
+    public DataSource dataSource() {
+        DriverManagerDataSource dataSource = new DriverManagerDataSource();
+        dataSource.setDriverClassName("org.postgresql.Driver");
+        dataSource.setUrl("jdbc:postgresql://db.rystiuryfkxbcqjtdplv.supabase.co:5432/postgres?user=postgres&password=[YOUR-PASSWORD]");
+        dataSource.setUsername("postgres");
+        dataSource.setPassword("SupaBase_1234");
+        return dataSource;
+    }
+}
---
+++ backend/workforce/src/main/java/cit/edu/workforce/service/DatabaseService.java
@@ -0,0 +1,20 @@
+package cit.edu.workforce.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Map;
+
+@Service
+public class DatabaseService {
+
+    @Autowired
+    private JdbcTemplate jdbcTemplate;
+
+    public List<Map<String, Object>> getData() {
+        String sql = "SELECT * FROM your_table";
+        return jdbcTemplate.queryForList(sql);
+    }
+}
