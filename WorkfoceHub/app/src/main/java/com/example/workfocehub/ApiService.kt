927dc41522b66ba6e30e3f808c326a5e10c1dc80
Katrina Amores <katrina.amores@cit.edu>
2025-04-25 21:43:34 +0800

    redo mobile

--- WorkfoceHub/app/src/main/java/com/example/workfocehub/ApiService.kt
+++
@@ -1,6 +0,0 @@
-package com.example.workfocehub
-
-
-interface ApiService {
-
-}
---
+++ workforce/settings.gradle.kts
@@ -0,0 +1,32 @@
+pluginManagement {
+    repositories {
+        google {
+            content {
+                includeGroupByRegex("com\\.android.*")
+                includeGroupByRegex("com\\.google.*")
+                includeGroupByRegex("androidx.*")
+            }
+        }
+        mavenCentral()
+        gradlePluginPortal()
+
+        maven{
+            url = uri("https://pkgs.dev.azure.com/MicrosoftDeviceSDK/DuoSDK-Public/_packaging/Duo-SDK-Feed/maven/v1")
+        }
+
+
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+        maven {
+            url = uri("https://pkgs.dev.azure.com/MicrosoftDeviceSDK/DuoSDK-Public/_packaging/Duo-SDK-Feed/maven/v1")
+        }
+    }
+}
+
+rootProject.name = "Workforce_Hub"
+include(":app")
